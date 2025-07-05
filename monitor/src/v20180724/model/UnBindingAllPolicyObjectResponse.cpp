63d2848b9bab422afdcb7ae2503e17bb91ba6729
tencentcloudapi <tencentcloudapi@tencent.com>
2025-07-06 00:07:26 +0800

    release 3.1.35

--- monitor/src/v20180724/model/UnBindingAllPolicyObjectResponse.cpp
+++ monitor/src/v20180724/model/UnBindingAllPolicyObjectResponse.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 THL A29 Limited, a Tencent company. All Rights Reserved.
+ * Copyright (c) 2017-2025 Tencent. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -75,7 +75,7 @@ string UnBindingAllPolicyObjectResponse::ToJsonString() const
     string key = "RequestId";
     iKey.SetString(key.c_str(), allocator);
     value.AddMember(iKey, rapidjson::Value().SetString(GetRequestId().c_str(), allocator), allocator);
-    
+
     rapidjson::StringBuffer buffer;
     rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
     value.Accept(writer);
